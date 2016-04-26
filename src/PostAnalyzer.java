bbd5bf62a009be0cf480c3fb9cd30cfc27da7e9f
nahidcse05 <mr4xb@virginia.edu>
2016-04-25 22:37:08 -0400

    First Commit

---
+++ src/PostAnalyzer.java
@@ -0,0 +1,336 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.text.Normalizer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+
+import org.tartarus.snowball.SnowballStemmer;
+import org.tartarus.snowball.ext.englishStemmer;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import structures.MyPriorityQueue;
+import structures._RankItem;
+
+public class PostAnalyzer {
+	
+	protected int m_Ngram; 
+	protected Tokenizer m_tokenizer;
+	protected SnowballStemmer m_stemmer;
+	HashSet<String> m_stopwords;
+	
+	HashMap<String, String> posts;
+	//HashMap<String, String [] > postsArray;
+	
+	HashMap<String, languageModel> languagModelList;
+	
+	
+	PostAnalyzer(){
+		posts = new HashMap<String, String>();
+		//postsArray = new HashMap<String, String[]>();
+		languagModelList = new HashMap <String, languageModel>();
+		
+		m_Ngram = 1;
+		try {
+			m_tokenizer = new TokenizerME(new TokenizerModel(new FileInputStream("./data/Model/en-token.bin")));
+			m_stemmer = new englishStemmer();
+			m_stopwords = new HashSet<String>();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void LoadStopwords(String filename) {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "UTF-8"));
+			String line;
+
+			while ((line = reader.readLine()) != null) {
+				line = SnowballStemming(Normalize(line));
+				if (!line.isEmpty())
+					m_stopwords.add(line);
+			}
+			reader.close();
+			System.out.format("Loading %d stopwords from %s\n", m_stopwords.size(), filename);
+		} catch(IOException e){
+			System.err.format("[Error]Failed to open file %s!!", filename);
+		}
+	}
+	
+	//Tokenizer.
+	protected String[] Tokenizer(String source){
+		String[] tokens = m_tokenizer.tokenize(source);
+		return tokens;
+	}
+	
+	//Snowball Stemmer.
+	protected String SnowballStemming(String token){
+		m_stemmer.setCurrent(token);
+		if(m_stemmer.stem())
+			return m_stemmer.getCurrent();
+		else
+			return token;
+	}
+	
+	//Normalize.
+	protected String Normalize(String token){
+		token = Normalizer.normalize(token, Normalizer.Form.NFKC);
+		token = token.replaceAll("\\W+", "");
+		token = token.toLowerCase();
+		
+		if (isNumber(token))
+			return "NUM";
+		else
+			return token;
+	}
+	
+	public boolean isNumber(String token) {
+		return token.matches("\\d+");
+	}	
+	
+	protected boolean isLegit(String token) {
+		return !token.isEmpty() 
+			&& !m_stopwords.contains(token)
+			&& token.length()>1
+			&& token.length()<20;
+	}
+	
+	protected boolean isBoundary(String token) {
+		return token.isEmpty();//is this a good checking condition?
+	}
+	
+	//Given a long string, tokenize it, normalie it and stem it, return back the string array.
+	protected String[] TokenizerNormalizeStemmer(String source){
+		String[] tokens = Tokenizer(source); //Original tokens.
+		//Normalize them and stem them.		
+		for(int i = 0; i < tokens.length; i++)
+			tokens[i] = SnowballStemming(Normalize(tokens[i]));
+		
+		LinkedList<String> Ngrams = new LinkedList<String>();
+		int tokenLength = tokens.length, N = m_Ngram;		
+		for(int i=0; i<tokenLength; i++) {
+			String token = tokens[i];
+			boolean legit = isLegit(token);
+			if (legit)
+				Ngrams.add(token);//unigram
+			
+			//N to 2 grams
+			if (!isBoundary(token)) {
+				for(int j=i-1; j>=Math.max(0, i-N+1); j--) {	
+					if (isBoundary(tokens[j]))
+						break;//touch the boundary
+					
+					token = tokens[j] + "-" + token;
+					legit |= isLegit(tokens[j]);
+					if (legit)//at least one of them is legitimate
+						Ngrams.add(token);
+				}
+			}
+		}
+		
+		return Ngrams.toArray(new String[Ngrams.size()]);
+	}
+	
+	
+	public void loadQuestionFile(File file) {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file.getAbsoluteFile()), "UTF-8"));
+			StringBuffer buffer = new StringBuffer(1024);
+			String line;
+
+			while((line=reader.readLine())!=null) {
+				buffer.append(line);
+			}
+			reader.close();
+			//System.out.println(Jsoup.parse(buffer.toString()).text());
+			
+			String key = questionNumberExtractor(file.getName());
+			if(!posts.containsKey(key))
+				posts.put(key, Jsoup.parse(buffer.toString()).text()+" #####"); // ##### end of question
+			else{
+				String tmp = posts.get(key) + " "+ Jsoup.parse(buffer.toString()).text();
+				posts.put(key, tmp);
+			}
+		} catch (IOException e) {
+			System.err.format("[Error]Failed to open file %s!", file.getName());
+			e.printStackTrace();
+		} 
+	}
+	
+	
+	public void loadAnswerFile(File file) {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file.getAbsoluteFile()), "UTF-8"));
+			StringBuffer buffer = new StringBuffer(1024);
+			String line;
+
+			while((line=reader.readLine())!=null) {
+				buffer.append(line);
+			}
+			reader.close();
+			//System.out.println(Jsoup.parse(buffer.toString()).text());
+			
+			String key = answerNumberExtractor(file.getName());
+			if(!posts.containsKey(key))
+				posts.put(key, Jsoup.parse(buffer.toString()).text());
+			else{
+				//System.out.println("Found");
+				String tmp = posts.get(key) + " "+ Jsoup.parse(buffer.toString()).text();
+				posts.put(key, tmp);
+			}
+		} catch (IOException e) {
+			System.err.format("[Error]Failed to open file %s!", file.getName());
+			e.printStackTrace();
+		} 
+	}
+	
+	public String questionNumberExtractor(String fileName){
+		int startIndex = fileName.indexOf("n") + 1;
+		int endIndex = fileName.indexOf(".");
+		
+		return fileName.substring(startIndex, endIndex);
+	}
+	
+	public String answerNumberExtractor(String fileName){
+		int startIndex = fileName.indexOf("r") + 1;
+		int endIndex = fileName.indexOf("_");
+		
+		return fileName.substring(startIndex, endIndex);
+	}
+	
+	public void LoadQuestionDirectory(String folder, String suffix) {
+		int postCounter = 0;
+		File dir = new File(folder);
+		for (File f : dir.listFiles()) {
+			if (postCounter>10000)
+				break;
+			if (f.isFile() && f.getName().endsWith(suffix)){
+				//analyzeDocumentDemo(LoadJson(f.getAbsolutePath()));
+				//System.out.println(f.getName());
+				//System.out.println(questionNumberExtractor(f.getName()));
+				loadQuestionFile(f);
+				postCounter++;
+			}
+			else if (f.isDirectory())
+				LoadQuestionDirectory(f.getAbsolutePath(), suffix);
+		}
+		System.out.println("Loading " + postCounter + " post documents from " + folder);
+	}
+	
+	public void LoadAnswerDirectory(String folder, String suffix) {
+		int postCounter = 0;
+		File dir = new File(folder);
+		for (File f : dir.listFiles()) {
+			if (postCounter>10000)
+				break;
+			if (f.isFile() && f.getName().endsWith(suffix)){
+				//analyzeDocumentDemo(LoadJson(f.getAbsolutePath()));
+				//System.out.println(f.getName());
+				//System.out.println(answerNumberExtractor(f.getName()));
+				loadAnswerFile(f);
+				postCounter++;
+			}
+			else if (f.isDirectory())
+				LoadAnswerDirectory(f.getAbsolutePath(), suffix);
+		}
+		System.out.println("Loading " + postCounter + " answer documents from " + folder);
+	}
+	
+	
+	public void generatePosts(){
+		Iterator it = posts.entrySet().iterator();
+		int fileIndex = 0;
+	    while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        //System.out.println(pair.getKey() + " = " + pair.getValue());
+	        //String[] tmp = TokenizerNormalizeStemmer(pair.getValue().toString());
+	        /*for(String str:tmp){
+	        	System.out.print(str+" ");
+	        }
+	        System.out.println();*/
+	        if(fileIndex%100!=0){ // for training
+		      
+	        	if(pair.getValue().toString().split("#####").length<=1)
+	        		continue;
+	        	if(TokenizerNormalizeStemmer(pair.getValue().toString()).length<20)
+	        		continue;
+		        languageModel langModel = new languageModel(TokenizerNormalizeStemmer(pair.getValue().toString()));
+		        langModel.unigramModel();
+		        langModel.bigramModel();
+		        
+		        languagModelList.put(pair.getKey().toString(), langModel);
+		        
+		    }
+	        fileIndex++;
+	    }
+	}
+	
+	public void test(){
+		
+		Iterator it = posts.entrySet().iterator();
+		int fileIndex = 0;
+	    while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        //System.out.println(pair.getKey() + " = " + pair.getValue());
+	        //String[] tmp = TokenizerNormalizeStemmer(pair.getValue().toString());
+	        /*for(String str:tmp){
+	        	System.out.print(str+" ");
+	        }
+	        System.out.println();*/
+	        if(fileIndex%100==0){ // for testing
+	        	String question = pair.getValue().toString().split("#####")[0];
+	        	System.out.println("Query::"+question);
+		        String [] testSentence = TokenizerNormalizeStemmer(question);
+		        MyPriorityQueue<_RankItem> fVector = new MyPriorityQueue<_RankItem>(5);
+				
+		        Iterator langModelit = languagModelList.entrySet().iterator();
+		        while (langModelit.hasNext()) {
+		        	 Map.Entry langpair = (Map.Entry)langModelit.next();
+		        	 languageModel langModel = (languageModel) langpair.getValue();
+		        	 double likelihood = langModel.estimateBiGram(testSentence);
+		        	 fVector.add(new _RankItem(langpair.getKey().toString(), likelihood));
+				}
+		        System.out.println("\n\n Ranked Results\n\n");
+		        for(_RankItem rt:fVector){
+					System.out.print(rt.m_name+"("+rt.m_value+")");
+					System.out.println(posts.get(rt.m_name));
+		        }
+				System.out.println("\n\n");
+			
+		        
+		    }
+	        fileIndex++;
+	    }
+		
+		
+	}
+	
+	public static void main(String[] args){
+		PostAnalyzer post = new PostAnalyzer();
+		post.LoadQuestionDirectory("/media/nahid/502C45162C44F90C/askUbuntu/outputFiles/question/", ".txt");
+		post.LoadAnswerDirectory("/media/nahid/502C45162C44F90C/askUbuntu/outputFiles/answer/", ".txt");
+		post.generatePosts();
+		post.test();
+	}
+	
+}
---
+++ src/languageModel.java
@@ -0,0 +1,151 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class languageModel {
+	private HashMap<String, Double> featureNameIndex;
+	private HashMap<String, Double> biFeatureNameIndex;
+	
+	private HashMap<String, Double> biFeatureNameIndexProb;
+	private double uniSum = 0.0;
+	private double lambda = 0.9;
+	
+	public languageModel(String[] tokens) {
+		featureNameIndex = new HashMap<String, Double>();
+		biFeatureNameIndex = new HashMap<String, Double>();
+		biFeatureNameIndexProb = new HashMap<String, Double>();
+		
+		int inword=1;
+		String prevToken="";
+		
+		double value = 0;
+		for (String token : tokens) {
+			if(token=="" || token==null)
+				continue;
+			// Bi-GramStart
+			if(inword==1){
+				inword=0;
+				prevToken=token;
+			}
+			else{
+				String bigram=prevToken+"@"+token;      //Bi-GramSet
+				value = 1.0;
+				if (biFeatureNameIndex.containsKey(bigram)) {
+					value = biFeatureNameIndex.get(bigram) + 1.0;
+				} 
+				biFeatureNameIndex.put(bigram,value);
+				prevToken=token;
+			}
+			
+	
+			// Unigram Starts
+			value = 1.0;
+			if (featureNameIndex.containsKey(token)) {
+				value = featureNameIndex.get(token) + 1.0;
+			}
+			featureNameIndex.put(token, value);
+		}
+	}
+	
+	public void unigramModel(){
+		Iterator it = featureNameIndex.entrySet().iterator();
+		double sum = 0.0;
+	    while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        //System.out.println("Key="+pair.getKey()+" ,Value="+pair.getValue());
+	        sum += (double)pair.getValue();
+	    }
+	    
+	    System.out.println("Sum:"+ sum);
+	    uniSum = sum;
+	    //probability assign
+	    /*it = featureNameIndex.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry)it.next();
+			featureNameIndex.put(pair.getKey().toString(), (double)pair.getValue()/sum);
+	    }*/
+	}
+	
+	public void bigramModel(){
+		
+		/*Iterator it = biFeatureNameIndex.entrySet().iterator();
+		double sum = 0.0;
+	    while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        //System.out.println("Key="+pair.getKey()+" ,Value="+pair.getValue());
+	        sum += (double)pair.getValue();
+	    }
+	    */
+	    //System.out.println("Bi Sum:"+ sum);
+	    //System.out.println("Uni Sum:"+ uniSum);
+	    double biSum = 0.0;
+	    double biProb = 0.0;
+	    String key ="";
+	    Iterator it = biFeatureNameIndex.entrySet().iterator();
+		while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        key = pair.getKey().toString();
+	        String prevToken = key.split("@")[0];
+	        double prevTokenCount = featureNameIndex.get(prevToken);
+	        biProb = (1-lambda)*(biFeatureNameIndex.get(key)/prevTokenCount) + lambda*(prevTokenCount/uniSum);
+	        biFeatureNameIndexProb.put(key, biProb);
+	        biSum+=biProb;
+	        //System.out.println("Prev Token:"+prevToken+", Count:"+prevTokenCount+", proba:"+(prevTokenCount/uniSum));
+	        //System.out.println("Bi Gram:"+key+", count:"+biFeatureNameIndex.get(key));
+	        //System.out.println("Key="+key+" ,Value="+biProb);
+	        
+		}
+	    
+		System.out.println("Bi Sum:"+ biSum);
+	}
+	
+	public double estimateBiGram(String[] tokens){
+		
+		HashMap<String, Double> biGramList = new HashMap<String, Double>();
+		
+		int inword=1;
+		String prevToken="";
+		
+		double value = 0;
+		for (String token : tokens) {
+			if(token=="" || token==null)
+				continue;
+			// Bi-GramStart
+			if(inword==1){
+				inword=0;
+				prevToken=token;
+			}
+			else{
+				String bigram=prevToken+"@"+token;      //Bi-GramSet
+				if (biGramList.containsKey(bigram)) {
+					value = biGramList.get(bigram) + 1.0;
+				} 
+				biGramList.put(bigram,value);
+				prevToken=token;
+			}
+		}
+		
+		Iterator it = biGramList.entrySet().iterator();
+		double likelihood = 1.0;
+	    while (it.hasNext()) {
+	    	 Map.Entry pair = (Map.Entry)it.next();
+	    	 String key = pair.getKey().toString();
+	    	 if(biFeatureNameIndex.containsKey(key))
+	    		 likelihood*=biFeatureNameIndexProb.get(key);
+	    	 else{
+	    		 prevToken = key.split("@")[0];
+	    		 if(featureNameIndex.containsKey(prevToken)){
+		 	         double prevTokenCount = featureNameIndex.get(prevToken);
+		 	         likelihood*=(prevTokenCount/uniSum);
+		    	}
+	    	 }
+	    }
+	    
+	    //System.out.println("Likelihood:"+likelihood);
+	    if(likelihood==1.0)
+	    	return Double.MIN_VALUE;
+	    return likelihood;
+	    
+	}
+}
